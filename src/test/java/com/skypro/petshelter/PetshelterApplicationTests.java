df5b1840057a793f3d067bdcbca5bf9fe54c31ca
sharayura <sharapyura@gmail.com>
2023-04-01 14:46:20 +0300

    Initial commit

---
+++ src/test/java/com/skypro/petshelter/PetshelterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.petshelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshelterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
